1252315d4f02e38f01b701b6d5d0de61c357d661
zenna <zenna@sigma.(none)>
2013-01-29 13:20:24 -0700

    added ardunino init

--- b2/billy.py
+++ b2/billy.py
@@ -33,7 +33,7 @@ class Billy:
     def init_arduino(self):
         #setup arduino
         self.ard = arduino.Arduino()
-        self.motor_right = arduino.Motor(ard, 0, 42, 9, False)
+        self.motor_right = arduino.Motor(self.ard, 0, 42, 9, False)
         self.motor_left = arduino.Motor(self.ard, 0, 48, 8, True)
         self.roller = arduino.Motor(self.ard, 0, 36, 10, True)
 
