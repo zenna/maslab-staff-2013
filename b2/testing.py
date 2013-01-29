b4d7079308a786074926800f6a9804749e86542e
zenna <zenna@sigma.(none)>
2013-01-29 14:22:48 -0700

    update testing

--- b2/testing.py
+++ b2/testing.py
@@ -2,47 +2,47 @@ import sys
 sys.path.append("..")
 import time
 
+from billy import *
+
 import arduino
 
 ard = arduino.Arduino()
 sys.path.append("..")
 
-motor_left = arduino.Motor(ard, 0, 42, 9, True)
-motor_right = arduino.Motor(ard, 0, 38, 10, True)
-roller = arduino.Motor(ard, 0, 48, 8, True)
-ard.run() # Start the Arduino communication thread
+billy = Billy()
+billy.init_arduino()
 
 def go_fwd(time_to_move):
 	print "fwd"
-	motor_left.setSpeed(50)
-	motor_right.setSpeed(50)
+	billy.motor_left.setSpeed(50)
+	billy.motor_right.setSpeed(50)
 	time.sleep(time_to_move)
-	motor_left.setSpeed(0)
-	motor_right.setSpeed(0)
+	billy.motor_left.setSpeed(0)
+	billy.motor_right.setSpeed(0)
 
 def go_bkwd(time_to_move):
 	print "bkwd"
-	motor_left.setSpeed(-50)
-	motor_right.setSpeed(-50)
+	billy.motor_left.setSpeed(-50)
+	billy.motor_right.setSpeed(-50)
 	time.sleep(time_to_move)
-	motor_left.setSpeed(0)
-	motor_right.setSpeed(0)
+	billy.motor_left.setSpeed(0)
+	billy.motor_right.setSpeed(0)
 
 def turn_left(time_to_move):
 	print "left"
-	motor_left.setSpeed(50)
-	motor_right.setSpeed(-50)
+	billy.motor_left.setSpeed(50)
+	billy.motor_right.setSpeed(-50)
 	time.sleep(time_to_move)
-	motor_left.setSpeed(0)
-	motor_right.setSpeed(0)
+	billy.motor_left.setSpeed(0)
+	billy.motor_right.setSpeed(0)
 
 def turn_right(time_to_move):
 	print "right"
-	motor_left.setSpeed(-50)
-	motor_right.setSpeed(50)
+	billy.motor_left.setSpeed(-50)
+	billy.motor_right.setSpeed(50)
 	time.sleep(time_to_move)
-	motor_left.setSpeed(0)
-	motor_right.setSpeed(0)
+	billy.motor_left.setSpeed(0)
+	billy.motor_right.setSpeed(0)
 
 
 # if __name__ == "__main__":
