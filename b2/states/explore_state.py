76e97dfb5ad87b78ab823c9f00b6a57b563514c3
Zenna Tavares <zennatavares@gmail.com>
2013-01-31 12:37:52 -0500

    test compass

--- b2/states/explore_state.py
+++ b2/states/explore_state.py
@@ -1,34 +1,64 @@
 from common import *
 from state_machine import *
+from pid import *
 
 #State :  explore, look for balls
 def explore_body(global_memory, local_memory, act, env):
 	# Do one full revolution using IMU
 	print "exploring"
-	sm_id = env["sync_value"]["state_machine_id"]
-	current_orientation = env["pull_value"](sm_id,"get_imu")[0]
-	one_eight_deg = (current_orientation + 180) % 360
+	# sm_id = env["sync_value"]["state_machine_id"]
+	img_thresh = env["sync_value"]["img"]
+	# img =
 
-	turn_to_orient(act, env, one_eight_deg, 20)
+	x,y = find_centroid(img_thresh,  global_mem["cam_width"],  global_mem["cam_height"],  global_mem["indices_x"],  global_mem["indices_y"])
+	print "Centroid:", x,y
+	draw_crosshairs(x,y, img_thresh)
 
-	import ipdb
-	ipdb.set_trace()
+	if y < 330:
+	    print "rotateleft"
+	    billy.roller.setSpeed(0)
+	    billy.motor_right.setSpeed(20)
+	    billy.motor_left.setSpeed(-20)
+	    time.sleep(.1)
+	    billy.motor_right.setSpeed(0)
+	    billy.motor_left.setSpeed(0)
+	elif y > 470:
+	    print "rotateright"
+	    billy.roller.setSpeed(0)
+	    billy.motor_right.setSpeed(-20)
+	    billy.motor_left.setSpeed(20)
+	    time.sleep(.1)
+	    billy.motor_right.setSpeed(0)
+	    billy.motor_left.setSpeed(0)
+	else:
+	    time.sleep(10)
 
-	turn_left(act)
-	orients = []
-	irs = []
-	while True:
-		env["pull_value"](sm_id,"get_imu")[0]
-		orients.append(env["pull_value"](sm_id,"get_imu")[0])
-		irs.append(env["pull_value"](sm_id,"get_imu")[0])
+	## OLD
 
-		if abs(current_orientation - one_eight_deg) < 5:
-			break
 
-	time.sleep(1)
-	orient_most_dist = orients[irs.index(max(irs))] 
-	turn_to_orient(act, env, orient_most_dist, 20)
-	time.sleep(5)
+	# current_orientation = env["pull_value"](sm_id,"get_imu")[0]
+	# one_eight_deg = (current_orientation + 180) % 360
+
+	# turn_to_orient(act, env, one_eight_deg, 20)
+
+	# import ipdb
+	# ipdb.set_trace()
+
+	# turn_left(act)
+	# orients = []
+	# irs = []
+	# while True:
+	# 	env["pull_value"](sm_id,"get_imu")[0]
+	# 	orients.append(env["pull_value"](sm_id,"get_imu")[0])
+	# 	irs.append(env["pull_value"](sm_id,"get_imu")[0])
+
+	# 	if abs(current_orientation - one_eight_deg) < 5:
+	# 		break
+
+	# time.sleep(1)
+	# orient_most_dist = orients[irs.index(max(irs))] 
+	# turn_to_orient(act, env, orient_most_dist, 20)
+	# time.sleep(5)
 
 explore_state = State(explore_body, no_propagators)
 # explore_propagators = [go_to_ready, go_to_avoid_wall]
