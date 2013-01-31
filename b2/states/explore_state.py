d5c4c258e3703329ebccb634ebd97394f6344082
Zenna Tavares <zennatavares@gmail.com>
2013-01-31 00:12:32 -0500

    initial explore code

--- b2/states/explore_state.py
+++ b2/states/explore_state.py
@@ -6,19 +6,14 @@ def explore_body(global_memory, local_memory, act, env):
 	# Do one full revolution using IMU
 	print "exploring"
 	sm_id = env["sync_value"]["state_machine_id"]
-	current_orientation = env["pull_value"](sm_id,"get_ir")
-	import ipdb
-	ipdb.set_trace()
+	current_orientation = env["pull_value"](sm_id,"get_imu")[3]
 	turn_left(act, 30)
-	# while True:
-
-	# 	go_to_state.
-	# 	if helpers.check_propositions() == True:
-	# 		return
-	# 	if done_full_circle
-	# 		break
-
-	# stop_wheels(act)
+	while True:
+		new_orient = env["pull_value"](sm_id,"get_imu")[3]
+		if abs(old_orient - new_orient) < 20:
+			stop_wheels(act)
+			time.sleep(5)
+			break
 
 explore_state = State(explore_body, no_propagators)
 # explore_propagators = [go_to_ready, go_to_avoid_wall]
