500f3e02481dff9aed7f33263863b2c107fb1651
EMILY MACKEVICIUS <elm@EMILYs-MacBook-Air.local>
2013-02-01 15:44:38 -0500

    avoiding walls, switching <

--- b2/states/avoidwalls_state.py
+++ b2/states/avoidwalls_state.py
@@ -5,7 +5,7 @@ def close_to_wall_prop(global_memory, local_memory, rcvd_msg, env):
 	# change this to an or of the left and right sensors
 	sm_id = env["sync_value"]["state_machine_id"]
 	ir_tuple= env["pull_value"](sm_id, "get_ir")
-	if ir_tuple[0] < WALL_THRESHOLD or ir_tuple[2] < WALL_THRESHOLD:
+	if ir_tuple[0] > WALL_THRESHOLD or ir_tuple[2] > WALL_THRESHOLD:
 		return True
 	else:
 		return False
