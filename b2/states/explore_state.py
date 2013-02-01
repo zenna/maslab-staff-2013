3211a35e5c33bf3f4307c3dd8e78a0a5eb02636c
Zenna Tavares <zennatavares@gmail.com>
2013-02-01 13:15:57 -0500

    test compass

--- b2/states/explore_state.py
+++ b2/states/explore_state.py
@@ -33,6 +33,8 @@ def explore_body(global_mem, local_mem, act, env, check_props):
 
         sm_id = env["sync_value"]["state_machine_id"]
         img_thresh = env["pull_value"](sm_id, "img")
+        stop_wheels()
+        time.sleep(.2)
         x,y = find_centroid(img_thresh,  global_mem["cam_width"],  global_mem["cam_height"],  global_mem["indices_x"],  global_mem["indices_y"])
 
         if 370 < y < 450:
