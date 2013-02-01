a2626ae333ff934907eb902aad5e6b117861b473
Zenna Tavares <zennatavares@gmail.com>
2013-01-31 21:58:33 -0500

    test compass

--- b2/states/explore_state.py
+++ b2/states/explore_state.py
@@ -38,8 +38,8 @@ def explore_pid_body(global_mem, local_mem, act, env):
     img_thresh = env["sync_value"]["img"]
 
     x,y = find_centroid(img_thresh,  global_mem["cam_width"],  global_mem["cam_height"],  global_mem["indices_x"],  global_mem["indices_y"])
-    print "Centroid:", x,y
-    draw_crosshairs(x,y, img_thresh)
+    # print "Centroid:", x,y
+    # draw_crosshairs(x,y, img_thresh)
 
     past_errors = global_mem["past_errors"]
     error_current = position_error(x,y)
@@ -57,32 +57,29 @@ def explore_pid_body(global_mem, local_mem, act, env):
     move_differential(controller_out,global_mem, act)
 
 def explore_compass_body(global_mem, local_mem, act, env):
-    pass
-    # current_orientation = env["pull_value"](sm_id,"get_imu")[0]
-    # one_eight_deg = (current_orientation + 180) % 360
+    current_orientation = env["pull_value"](sm_id,"get_imu")[0]
+    one_eight_deg = (current_orientation + 180) % 360
 
-    # turn_to_orient(act, env, one_eight_deg, 20)
 
-    # import ipdb
-    # ipdb.set_trace()
+    turn_to_orient(act, env, one_eight_deg, 20)
 
-    # turn_left(act)
-    # orients = []
-    # irs = []
-    # while True:
-    #   env["pull_value"](sm_id,"get_imu")[0]
-    #   orients.append(env["pull_value"](sm_id,"get_imu")[0])
-    #   irs.append(env["pull_value"](sm_id,"get_imu")[0])
+    turn_left(act)
+    orients = []
+    irs = []
+    while True:
+      env["pull_value"](sm_id,"get_imu")[0]
+      orients.append(env["pull_value"](sm_id,"get_imu")[0])
+      irs.append(env["pull_value"](sm_id,"get_imu")[0])
 
-    #   if abs(current_orientation - one_eight_deg) < 5:
-    #       break
+      if abs(current_orientation - one_eight_deg) < 5:
+          break
 
-    # time.sleep(1)
-    # orient_most_dist = orients[irs.index(max(irs))] 
-    # turn_to_orient(act, env, orient_most_dist, 20)
-    # time.sleep(5)
+    time.sleep(1)
+    orient_most_dist = orients[irs.index(max(irs))] 
+    turn_to_orient(act, env, orient_most_dist, 20)
+    time.sleep(5)
 
 find_ball_prop = [{'proposition':am_init, 'dst_state_id':"explore"}]
-find_ball_state = State(explore_body,find_ball_prop)
+find_ball_state = State(explore_compass_body,find_ball_prop)
 explore_state = State(explore_body, [] )
 # explore_propagators = [go_to_ready, go_to_avoid_wall]
